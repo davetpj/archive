69796cccd5be9819ee61b94fac6b75684cdfa479
davetpj <dhkim92@icloud.com>
2021-07-27 23:29:56 +0900

    algo

--- greedy algorithm/cu_3321.py
+++ greedy algorithm/cu_3321.py
@@ -1,24 +1,12 @@
-n = int(input())
-dough, topping = map(int, input().split())
-dough_cal = int(input())
-topping_cals = []
-for _ in range(n):
-    topping_cal = int(input())
-    topping_cals.append(topping_cal)
+# n = int(input())
+# dough, topping = map(int, input().split())
+# dough_cal = int(input())
+# topping_cals = []
+# for _ in range(n):
+#     topping_cal = int(input())
+#     topping_cals.append(topping_cal)
 
-menus = [dough_cal/dough]
-# for i in range(n):
-#     if i == 0:# 토핑 한가지
-#        menu = ((dough_cal + topping_cals[i])/(dough+topping))
-#        menus.append(menu)
+# menus = [dough_cal/dough]
 
-
-# dough_cal/dough
-# (dough_cal + topping_cals[0])/(dough+topping)
-# (dough_cal + topping_cals[1])/(dough+topping)
-# (dough_cal + topping_cals[2])/(dough+topping)
-# (dough_cal + topping_cals[0] + topping_cals[1])/(dough+topping+topping)
-# (dough_cal + topping_cals[0] + topping_cals[2])/(dough+topping+topping)
-# (dough_cal + topping_cals[1] + topping_cals[2])/(dough+topping+topping)
-# (dough_cal + topping_cals[0] + topping_cals[1] +
-#  topping_cals[2])/(dough+topping+topping+topping)
+my_list = ['a', 'b', 'c', 'd']
+print(my_list+['a', 'a', 'a', 'a'])
